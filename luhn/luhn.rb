c1544cf770088f8f09dc610575688045630e2e50
nilventosa <ventosa@gmail.com>
2025-01-02 21:18:45 +0100

    Mentoring comments

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,14 +1,30 @@
 class Luhn
-  def self.valid?(input)
-    clean_input = input.gsub(/\s/, '')
-    return false unless clean_input.size > 1
-    return false if clean_input =~ /[^\d]/
 
-    reversed_chars = clean_input.chars.map! { |t| t.to_i }.reverse
-    doubled = reversed_chars.map.with_index { |e, i| 
-      t = (i % 2).zero? ? e : e * 2
-      t >= 10 ? t - 9 : t
-    }
-    (doubled.sum % 10).zero? ? true : false
+  MINIMUM_NUMBER_SIZE = 2
+
+  def self.valid?(identification_number)
+    clean_number = remove_spaces(identification_number)
+    return false unless can_number_be_validated?(clean_number) 
+
+    (run_luhn_algorithm(clean_number).sum % 10).zero?
   end
+
+  def self.run_luhn_algorithm(clean_number)
+    clean_number.chars.reverse.map!.with_index { |digit, i| 
+      digit = digit.to_i
+      digit = (i % 2).zero? ? digit : digit * 2
+      digit >= 10 ? digit - 9 : digit
+    }
+  end
+
+  def self.can_number_be_validated?(number)
+    number =~ /^\d+$/ && number.size >= MINIMUM_NUMBER_SIZE
+  end
+
+  def self.remove_spaces(string)
+    string.gsub(/\s/, '')
+  end
+
+  private_class_method :can_number_be_validated?, :run_luhn_algorithm, :remove_spaces
+
 end
