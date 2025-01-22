b083d5fb6ccdb2a4aaf8b1592e2ef5d156676523
nilventosa <ventosa@gmail.com>
2025-01-22 17:57:34 +0100

    robot-simulator: review comments

--- robot-simulator/robot_simulator.rb
+++ robot-simulator/robot_simulator.rb
@@ -1,6 +1,6 @@
 class Robot
 
-  VALID_DIRECTIONS = %i[east south west north]
+  DIRECTION = %i[east south west north]
 
   private
 
@@ -10,13 +10,13 @@ class Robot
   end
 
   def turn(side)
-    index_modifier = { left: -1, right: 1 }[side] || raise(ArgumentError)
+    index_modifier = {left: -1, right: 1}[side] || raise(ArgumentError)
 
-    new_index = (VALID_DIRECTIONS.find_index(bearing) + index_modifier) % 4
-    orient(VALID_DIRECTIONS[new_index])
+    new_index = (DIRECTION.find_index(bearing) + index_modifier) % 4
+    orient(DIRECTION[new_index])
   end
 
-  private_constant :VALID_DIRECTIONS
+  private_constant :DIRECTION
 
   public
 
@@ -27,7 +27,7 @@ class Robot
   end
 
   def orient(bearing)
-    VALID_DIRECTIONS.include?(bearing) ? self.bearing = bearing : raise(ArgumentError)
+    DIRECTION.include?(bearing) ? self.bearing = bearing : raise(ArgumentError)
   end
 
   def turn_right
@@ -38,23 +38,27 @@ class Robot
     turn :left
   end
 
-def advance
-  movement = { north: [0, 1], east:  [1, 0], south: [0, -1], west:  [-1, 0] }
+  def advance
+    movement = {north: [0, 1], east:  [1, 0], south: [0, -1], west:  [-1, 0]}
 
-  dx, dy = movement[self.bearing]
-  self.coordinates[0] += dx
-  self.coordinates[1] += dy
-end
+    dx, dy = movement[self.bearing]
+    self.coordinates[0] += dx
+    self.coordinates[1] += dy
+  end
 
 end
 
 class Simulator
+    
+    LETTER_INSTRUCTION_MAP = {L: :turn_left, R: :turn_right, A: :advance}
 
   private
 
   def parse_instruction(letter)
-    { L: :turn_left, R: :turn_right, A: :advance }[letter.to_sym]
+    LETTER_INSTRUCTION_MAP[letter.to_sym]
   end
+
+  private_constant :LETTER_INSTRUCTION_MAP
 
   public
 
